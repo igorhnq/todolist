6f9d310133ad598f7b771ab3f9c837f978137616
igorhnq <igorhenrique102@gmail.com>
2024-11-09 11:12:57 -0300

    feat: add ResponseEntity handling for user creation

--- src/main/java/br/com/igorhnq/todolist/user/UserController.java
+++ src/main/java/br/com/igorhnq/todolist/user/UserController.java
@@ -1,10 +1,14 @@
 package br.com.igorhnq.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -14,15 +18,14 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            System.out.println("User already exists");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
